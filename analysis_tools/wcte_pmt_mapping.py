d5457dc78605c2971606a6b7cad5da3a77e21b1b
Nick Prouse <n.prouse@imperial.ac.uk>
2026-05-05 15:33:24 +0100

    Use sensible and much simplified PMT mapping to avoid looping over the dictionary, avoid converting strings and ints every time, and allow it to work with arrays

--- analysis_tools/wcte_pmt_mapping.py
+++ analysis_tools/wcte_pmt_mapping.py
@@ -1,35 +1,41 @@
 import json
 from importlib.resources import files
+import numpy as np
+
 
 class PMTMapping:
     def __init__(self):
         # Load the JSON from the package's data folder
         json_path = files('analysis_tools.data').joinpath('PMT_Mapping.json')
-        print("json path",json_path)
+        print("json path", json_path)
         with json_path.open('r') as file:
             self.pmt_data = json.load(file)["mapping"]
 
-    def get_positions_from_entry(self,long_form_id):
-        # longform ID is mpmt_id * 100 + pmt_id
-        return long_form_id // 100, long_form_id % 100
+        cards = np.array([int(k) // 100 for k in self.pmt_data.keys()])
+        channels = np.array([int(k) % 100 for k in self.pmt_data.keys()])
 
-    def get_key_from_value(self, input_value):
-        for key, value in self.pmt_data.items():
-            if value == input_value:
-                return key
-        return None  # No match found
-    
-    def get_slot_pmt_pos_from_card_pmt_chan(self,card_id,pmt_channel):
-        if str((100*card_id)+pmt_channel) in self.pmt_data:
-            slot_id, pmt_pos = self.get_positions_from_entry(self.pmt_data[str((100*card_id)+pmt_channel)])
-        else:
-            raise Exception("Card chan",card_id,pmt_channel,"not in mapping json")
+        slots = np.array([v // 100 for v in self.pmt_data.values()])
+        positions = np.array([v % 100 for v in self.pmt_data.values()])
+
+        unique_cards, _idx = np.unique(cards, return_index=True)
+        unique_slots = slots[_idx]
+
+        self.slot_from_card = np.full(np.max(unique_cards) + 1, -1)
+        self.card_from_slot = np.full(np.max(unique_slots) + 1, -1)
+        self.slot_from_card[unique_cards] = unique_slots
+        self.card_from_slot[unique_slots] = unique_cards
+
+        self.position_from_card_channel = np.full([np.max(cards) + 1, np.max(channels) + 1], -1)
+        self.channel_from_slot_position = np.full([np.max(slots) + 1, np.max(positions) + 1], -1)
+        self.position_from_card_channel[cards, channels] = positions
+        self.channel_from_slot_position[slots, positions] = channels
+
+    def get_slot_pmt_pos_from_card_pmt_chan(self, card_id, pmt_channel):
+        slot_id = self.slot_from_card[card_id]
+        pmt_pos = self.position_from_card_channel[card_id, pmt_channel]
         return slot_id, pmt_pos
-    
-    def get_card_pmt_chan_from_slot_pmt_pos(self,slot_id, pmt_pos):
-        value = ((100*slot_id)+pmt_pos)
-        key = self.get_key_from_value(value)
-        if key is None:
-            raise Exception("No card, chan found for slot",slot_id,"pmt pos",pmt_pos)
-        key = int(key)
-        return self.get_positions_from_entry(key)
+
+    def get_card_pmt_chan_from_slot_pmt_pos(self, slot_id, pmt_pos):
+        card_id = self.card_from_slot[slot_id]
+        channel_id = self.channel_from_slot_position[slot_id, pmt_pos]
+        return card_id, channel_id
